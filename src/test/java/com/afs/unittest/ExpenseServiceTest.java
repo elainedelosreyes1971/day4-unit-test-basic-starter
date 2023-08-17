17b5df3fd090b624babdf5d88c8a6060790cb84f
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-17 13:23:22 +0800

    refactor: made the variable names specific and clear for each testing

--- src/test/java/com/afs/unittest/ExpenseServiceTest.java
+++ src/test/java/com/afs/unittest/ExpenseServiceTest.java
@@ -7,21 +7,26 @@ class ExpenseServiceTest {
     @Test
     void should_return_internal_expense_type_when_getExpenseCodeByProject_given_internal_project() {
         // given
+        Project internalProject = new Project(ProjectType.INTERNAL, "Project Internal");
+        ProjectService projectService = new ProjectService();
+        ExpenseService expenseService = new ExpenseService(projectService);
 
         // when
+        ExpenseType expenseCodeByProject = expenseService.getExpenseCodeByProject(internalProject);
 
         // then
+        Assertions.assertEquals(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseCodeByProject);
     }
 
     @Test
     void should_return_expense_type_A_when_getExpenseCodeByProject_given_project_is_external_and_name_is_project_A() {
         // given
-        Project externalProject = new Project(ProjectType.EXTERNAL, "Project A");
+        Project externalProjectA = new Project(ProjectType.EXTERNAL, "Project A");
         ProjectService projectService = new ProjectService();
         ExpenseService expenseService = new ExpenseService(projectService);
 
         // when
-        ExpenseType expenseCodeByProject = expenseService.getExpenseCodeByProject(externalProject);
+        ExpenseType expenseCodeByProject = expenseService.getExpenseCodeByProject(externalProjectA);
 
         // then
         Assertions.assertEquals(ExpenseType.EXPENSE_TYPE_A, expenseCodeByProject);
@@ -30,12 +35,12 @@ class ExpenseServiceTest {
     @Test
     void should_return_expense_type_B_when_getExpenseCodeByProject_given_project_is_external_and_name_is_project_B() {
         // given
-        Project externalProject = new Project(ProjectType.EXTERNAL, "Project B");
+        Project externalProjectB = new Project(ProjectType.EXTERNAL, "Project B");
         ProjectService projectService = new ProjectService();
         ExpenseService expenseService = new ExpenseService(projectService);
 
         // when
-        ExpenseType expenseCodeByProject = expenseService.getExpenseCodeByProject(externalProject);
+        ExpenseType expenseCodeByProject = expenseService.getExpenseCodeByProject(externalProjectB);
 
         // then
         Assertions.assertEquals(ExpenseType.EXPENSE_TYPE_B, expenseCodeByProject);
@@ -44,12 +49,12 @@ class ExpenseServiceTest {
     @Test
     void should_return_other_expense_type_when_getExpenseCodeByProject_given_project_is_external_and_has_other_name() {
         // given
-        Project externalProject = new Project(ProjectType.EXTERNAL, "Other Project");
+        Project otherExternalProject = new Project(ProjectType.EXTERNAL, "Other Project");
         ProjectService projectService = new ProjectService();
         ExpenseService expenseService = new ExpenseService(projectService);
 
         // when
-        ExpenseType expenseCodeByProject = expenseService.getExpenseCodeByProject(externalProject);
+        ExpenseType expenseCodeByProject = expenseService.getExpenseCodeByProject(otherExternalProject);
 
         // then
         Assertions.assertEquals(ExpenseType.OTHER_EXPENSE, expenseCodeByProject);
